f6159c2fa11df7ffa6c598e0c5bc65e8b2a920f5
Khoa Nguyen <ngdoan.anhkhoa@gmail.com>
2020-03-26 20:18:53 +0700

    Updates

---
+++ precache-manifest.fa93ee0328c72a618c657c0357bbd9ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c659566bf50c921c197e2452a9769c2e",
+    "url": "/Highland/index.html"
+  },
+  {
+    "revision": "aa89025b13ed135b2181",
+    "url": "/Highland/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "aa89025b13ed135b2181",
+    "url": "/Highland/static/js/2.64b934df.chunk.js"
+  },
+  {
+    "revision": "4605dcf609db55a39422",
+    "url": "/Highland/static/js/main.7e19bc39.chunk.js"
+  },
+  {
+    "revision": "7853eb9d474540d852a9",
+    "url": "/Highland/static/js/runtime-main.32c2afa8.js"
+  }
+]);
